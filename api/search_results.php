21d1df9a11772aabebdbbede184b63c2b2427ea1
Alastair Otter <alastair@mediahack.co.za>
2017-10-22 16:19:43 +0200

    Adding front-end search

--- api/search_results.php
+++ api/search_results.php
@@ -6,6 +6,11 @@ include("config.php");
 
 
 
+$id = $_GET['id'];
+$section = $_GET['section'];
+
+
+
 $results = array();
 
 
@@ -30,18 +35,32 @@ $results = array();
 //        array_push($results,$row);
 //    }
 
+if($id && $section) { 
+if($section == "mayor") { $sql = "Select * from promises where mayor = '$id'"; }
+elseif($section == "municipality") { echo $section; echo $id; $sql = "Select * from promises where municipality = '$id'"; }
 
-
-$sql = "Select * from promises";
+//$sql = "Select * from promises";
     $query = mysqli_query($db, $sql);
     $promises = array();
     while($row = mysqli_fetch_assoc($query)) {
+        
+        
         // get mayor name
         $q = "select * from mayors where id = " . $row['mayor'];  
         $q = mysqli_query($db, $q);
         $q = mysqli_fetch_assoc($q);
         $muni = $q["municipality"];
         $row["mayor_name"] = $q["name"];
+        $row["mayor_photo"] = $q["photo"];
+        $row["party_id"] = $q["party"];
+        // get party name
+        // got party_id        
+        $q = "select * from parties where id = " . $row['party_id'];  
+        $q = mysqli_query($db, $q);
+        $q = mysqli_fetch_assoc($q);
+        $row["party_name"] = $q["name"];
+        $row["party_abbr"] = $q["abbreviation"];
+        $row["party_logo"] = $q["logo"];
         // get mayor municipality
         $q = "select * from municipalities where id = '$muni'";  
         $q = mysqli_query($db, $q);
@@ -57,7 +76,7 @@ $sql = "Select * from promises";
 
 echo json_encode($results);
     
-
+}
     
 
 ?>
--- app.js
+++ app.js
@@ -22,14 +22,34 @@ myApp.config(function($routeProvider) {
 
 myApp.controller('mainController', ['$scope', '$http', '$location', '$rootScope', function ($scope, $http, $location, $rootScope) {
 		
-        
+    $scope.searchResult = false;     
+    
          $http.get(baseUrl + "api/search_data.php")
                 .then( function(data) { 
             
                     $scope.search = data.data;
-             console.log($scope.search);
+//             console.log($scope.search);
 //                   
                      });
+    
+    
+        $scope.viewRecord = function (selectedRecord)  { 
+               console.log($scope.selectedRecord); 
+            
+            $scope.searchResult = true;
+            
+            $http.get(baseUrl + "api/search_results.php?id=" + $scope.selectedRecord.originalObject.id + "&section=" + $scope.selectedRecord.originalObject.type)
+                    .then( function(data) {
+               
+                    $scope.searchResultsTitle = data.data[0];
+                    $scope.searchResults = data.data;
+                    $scope.searchType = $scope.selectedRecord.originalObject.type;
+//                    console.log($scope.searchResults);
+                console.log($scope.searchResultsTitle);
+                
+            });
+            
+        }
 
        
 
